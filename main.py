c267ad16bc39f78a347d192d4a491fe6a8be9727
nathanalexanderpage <nathanalexanderpage@gmail.com>
2019-09-07 16:45:31 -0700

    reads and prints ratings, users, items using pandas

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # constants
+CSV_FILEPATH_BASE = os.getenv('CSV_FILEPATH_BASE')
 CSV_USERS_FILEPATH = os.getenv('CSV_USERS_FILEPATH')
 CSV_RATINGS_FILEPATH = os.getenv('CSV_RATINGS_FILEPATH')
 CSV_ITEMS_FILEPATH = os.getenv('CSV_ITEMS_FILEPATH')
+print('\nCSV filepaths are as follows:\n{}\n{}\n{}\n'.format(CSV_USERS_FILEPATH, CSV_RATINGS_FILEPATH, CSV_ITEMS_FILEPATH))
 
 # read user file
 u_cols = ['user_id', 'age', 'sex', 'occupation', 'zip_code']
@@ -25,3 +27,23 @@ i_cols = ['movie id', 'movie title' ,'release date','video release date', 'IMDb 
 'Film-Noir', 'Horror', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 'War', 'Western']
 items = pd.read_csv(CSV_ITEMS_FILEPATH, sep='|', names=i_cols,
 encoding='latin-1')
+
+# look at user data
+print('\nUser Data :')
+print('shape : ', users.shape)
+print(users.head())
+
+# look at ratings data
+print("\nRatings Data :")
+print("shape : ", ratings.shape)
+print(ratings.head())
+
+# look at items data
+print("\nItem Data :")
+print("shape : ", items.shape)
+print(items.head(6))
+
+ratings_train = pd.read_csv(CSV_FILEPATH_BASE + 'ua.base', sep='\t', names=r_cols, encoding='latin-1')
+ratings_test = pd.read_csv(CSV_FILEPATH_BASE + 'ua.test', sep='\t', names=r_cols, encoding='latin-1')
+print(ratings_train.shape, ratings_test.shape)
+
